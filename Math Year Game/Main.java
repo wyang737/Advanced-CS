61a05d90e03fafe1ed238dd83aae94a0ffdc2a51
Winston Yang <wyang737@users.noreply.github.com>
2018-03-03 00:20:30 -0500

    Update Main.java

--- Math Year Game/Main.java
+++ Math Year Game/Main.java
@@ -5,21 +5,7 @@ public class Main {
 	public static void main(String[] args){
 		ExpressionParser ep = new ExpressionParser();
 		ExpressionGenerator eg = new ExpressionGenerator("2018");
-		ArrayList<String> expressions = eg.getExpressions();
-		HashMap<Double, String> answers = new HashMap<>();
-		System.out.println("Answer: " + ep.getAnswer("8+2)))))"));
-		for (int j = 0; j < 100; j ++){
-			answers.put((double) j, "0");
-		}
-		for (int i = 0; i < expressions.size(); i ++){
-			String expression = expressions.get(i);
-			Double answer = ep.getAnswer(expression);
-			if (answers.containsKey(answer)){
-				answers.put(answer, expression);
-			}
-		}
-		for (int k = 0; k < answers.size(); k ++){
-			System.out.println(k + ": " + answers.get((double) k));
-		}  
+		ArrayList<String> expressions = eg.getExpressions()
+		 
 	}
 }
